eb991c9ebee0277fa1334b0caad02e51385bbd5f
Peter Shoukry <pshoukry@gmail.com>
2024-09-07 13:01:10 +0300

    chore: update default colorscheme

--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -8,13 +8,13 @@ return {
       local elixirls = require("elixir.elixirls")
 
       elixir.setup({
-        nextls = { enable = true },
+        nextls = { enable = false },
         credo = {},
         elixirls = {
           enable = true,
           settings = elixirls.settings({
             dialyzerEnabled = false,
-            enableTestLenses = false,
+            enableTestLenses = true,
           }),
           on_attach = function(client, bufnr)
             vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,56 @@
 return {
-  -- add gruvbox
   {
     "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
   },
   { "ellisonleao/gruvbox.nvim" },
   {
     "folke/tokyonight.nvim",
-    config = function()
-      vim.cmd([[colorscheme tokyonight-day]])
-    end,
   },
   { "sainnhe/everforest" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  },
 }
